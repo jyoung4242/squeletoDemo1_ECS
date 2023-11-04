1a661b887d0fae7d6ab03cefecde7d42f855f4d4
Justin Young <justin_dean_young@yahoo.com>
2023-11-04 11:42:05 -0400

    added audio

--- src/Events/walk.ts
+++ src/Events/walk.ts
@@ -21,6 +21,7 @@ import { Vector } from "../../_Squeleto/Vector";
 import { VelocityComponent } from "../Components/velocity";
 import { SpriteSheetComponent } from "../Components/spritesheet";
 import { PositionComponent } from "../Components/positionComponent";
+import { Signal } from "../../_Squeleto/Signals";
 
 export type direction = "right" | "left" | "up" | "down";
 
@@ -28,6 +29,7 @@ export class WalkEvent extends GameEvent {
   direction: direction;
   who: (Entity & VelocityComponent & SpriteSheetComponent & PositionComponent) | null;
   targetValue: number = 0;
+  actionSignal = new Signal("changePassiveState");
   distance: number;
   speed: number;
   resolution: ((value: void | PromiseLike<void>) => void) | undefined;
@@ -84,7 +86,9 @@ export class WalkEvent extends GameEvent {
 
     return new Promise(resolve => {
       this.resolution = resolve;
-      if (this.who)
+      if (this.who) {
+        //send Signal
+        this.actionSignal.send([this.who.id, "walk"]);
         switch (this.direction) {
           case "right":
             this.who.velocity = new Vector(this.speed, 0);
@@ -107,6 +111,7 @@ export class WalkEvent extends GameEvent {
             this.targetValue = this.who.position.y + this.distance;
             break;
         }
+      }
     });
   }
 }
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,10 @@
-export default {
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+
+export default defineConfig({
   base: "./",
-};
+  test: {
+    environment: "jsdom",
+    globals: true,
+  },
+});
